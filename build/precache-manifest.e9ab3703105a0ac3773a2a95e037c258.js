0af8d74713d45da54c3331925af95940c10e5dcf
basepath added <Maggie.Ryder@havas.com>
2019-03-27 15:24:28 -0400

    update build folder

---
+++ build/precache-manifest.e9ab3703105a0ac3773a2a95e037c258.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e74714c2c6108230c3f",
+    "url": "/hooks-drumr/static/js/runtime~main.5dddb1d5.js"
+  },
+  {
+    "revision": "5a1a40219e4169e56cc6",
+    "url": "/hooks-drumr/static/js/main.b382d3fb.chunk.js"
+  },
+  {
+    "revision": "10397f0a3d4917c6b2be",
+    "url": "/hooks-drumr/static/js/2.2a8e59c3.chunk.js"
+  },
+  {
+    "revision": "5a1a40219e4169e56cc6",
+    "url": "/hooks-drumr/static/css/main.26216cd7.chunk.css"
+  },
+  {
+    "revision": "3c8c65e432b5359f9750e8e21d9b0545",
+    "url": "/hooks-drumr/index.html"
+  }
+];
